1794b93dfd242a70bc7712d4c8e19dba53964637
limaojunio <ecjrcl@gmail.com>
2025-11-07 12:31:36 -0300

    Card de como funciona, Card de empresa, Carrossel de Empresas, Feedbacks e home page

--- src/components/comofunciona/CardComoFunciona.tsx
+++ src/components/comofunciona/CardComoFunciona.tsx
@@ -1,41 +1,57 @@
-import { BuildingOfficeIcon, DeviceMobileIcon, HandshakeIcon } from "@phosphor-icons/react";
+import { DeviceMobileIcon, HandshakeIcon } from "@phosphor-icons/react";
 
 function CardComoFunciona() {
   return (
     <>
-      <section className="bg-gray-200">
+      <section className="bg-blue-400">
         <div className="container mx-auto">
           <div className="px-8 py-8 lg:py-11">
-            <h2 className="w-[250px] text-2xl font-semibold md:w-full md:text-5xl lg:w-1/2 lg:leading-[130%]">
+            <h2 className="w-[250px] text-2xl inline-flex items-center justify-center font-semibold text-blue-50 md:w-full md:text-5xl lg:leading-[130%] ">
               Como funciona o SmartGen?
             </h2>
             <div className="mb-4 mt-6 grid grid-cols-1 gap-4 md:mb-6 md:grid-cols-3 md:gap-6">
-              <div className="flex flex-col rounded-2xl bg-blue-200 p-4">
+              {/* <div className="flex flex-col rounded-2xl bg-blue-200 p-4">
                 <BuildingOfficeIcon size={32} color="#4b80fb" />
-                <div className="flex flex-col gap-4">
-                  <h3>01. Empresas contratam</h3>
+                <div className="flex flex-col gap-4 ">
+                  <h3 className="text-3xl font-semibold">01. Empresas contratam</h3>
                   <p>
-                    Contrate a TotalPass para os seus colaboradores terem acesso
+                    Contrate a SmartGen para os seus colaboradores terem acesso
+                    aos serviços de bem-estar.
+                  </p>
+                </div>
+              </div> */}
+              <div className="flex flex-col rounded-2xl bg-blue-50 p-4">
+                <div className="flex flex-col gap-4">
+                  <DeviceMobileIcon size={32} color="#4b80fb" />
+                  <h3 className="text-3xl font-semibold">
+                    01. Empresas contratam
+                  </h3>
+                  <p>
+                    Contrate a SmartGen para os seus colaboradores terem acesso
                     aos serviços de bem-estar.
                   </p>
                 </div>
               </div>
-              <div className="flex flex-col rounded-2xl bg-blue-200 p-4">
+              <div className="flex flex-col rounded-2xl bg-blue-50 p-4">
                 <div className="flex flex-col gap-4">
                   <DeviceMobileIcon size={32} color="#4b80fb" />
-                  <h3>02. Colaboradores escolhem</h3>
+                  <h3 className="text-3xl font-semibold">
+                    02. Colaboradores escolhem
+                  </h3>
                   <p>
                     Seus colaboradores escolhem o plano que melhor combina com
                     cada estilo de vida.
                   </p>
                 </div>
               </div>
-              <div className="flex flex-col rounded-2xl bg-blue-200 p-4">
+              <div className="flex flex-col rounded-2xl bg-blue-50 p-4">
                 <div className="flex flex-col gap-4">
                   <HandshakeIcon size={32} color="#4b80fb" />
-                  <h3>03. Aproveitam o benefício</h3>
+                  <h3 className="text-3xl font-semibold">
+                    03. Aproveitam o benefício
+                  </h3>
                   <p>
-                    Com o plano ativo, é só acessar o app TotalPass e escolher
+                    Com o plano ativo, é só acessar o app SmartGen e escolher
                     entre as milhares de opções de academias e serviços de
                     bem-estar.
                   </p>
@@ -44,7 +60,7 @@ function CardComoFunciona() {
             </div>
             <div className="mx-auto flex max-w-md flex-col gap-5 md:flex-row bg-blue-500 rounded-[42px] text-white">
               <a
-                className="rounded-[42px] inline-flex items-center justify-center py-4 px-8 hover:text-neutral-90 text-base transition bg-primary-40 text-neutral-90 hover:bg-blue-600 w-full"
+                className="rounded-[42px] inline-flex items-center justify-center py-4 px-8 hover:text-neutral-90 text-base transition bg-primary-40 text-neutral-90 hover:bg-blue-900 w-full"
                 target="_self"
                 href="/br/corporativo/"
               >
--- src/components/empresa/cardEmpresa/CardEmpresa.tsx
+++ src/components/empresa/cardEmpresa/CardEmpresa.tsx
@@ -11,55 +11,59 @@ export default function CardEmpresa({
   showActions = true,
 }: CardEmpresaProps) {
   return (
-    <div className=" bg-gray-100 border-slate-900 border flex flex-col rounded overflow-hidden justify-between">
-      <div>
-        <div className="flex w-full bg-blue-200 py-2 px-4 items-center gap-4">
-          {/* <img
+    <section>
+      <div className="container mx-auto ">
+        <div className=" bg-gray-100 border border-blue-200 rounded-2xl flex flex-col rounded overflow-hidden justify-between ">
+          <div>
+            <div className="flex w-full bg-blue-400 py-2 px-4 items-center gap-4">
+              {/* <img
             src={empresa.usuario?.foto || "https://i.imgur.com/ZZFAmzo.jpg"}
             className="h-12 w-12 rounded-full object-cover"
             alt={empresa.usuario?.nome || "Empresa"}
           /> */}
-          <h3 className="text-lg font-bold text-center uppercase">
-            {empresa.nome_empresa}
-          </h3>
-        </div>
+              <h3 className="text-lg font-bold text-center uppercase">
+                {empresa.nome_empresa}
+              </h3>
+            </div>
 
-        <div className="p-4">
-          <p className="text-sm">
-            <span className="font-semibold">Localização: </span>
-            {empresa.localizacao}
-          </p>
-          <p className="text-sm">
-            <span className="font-semibold">Telefone: </span>
-            {empresa.telefone_contato}
-          </p>
-          <p className="text-sm break-words">
-            <span className="font-semibold">Instagram: </span>
-            {empresa.instagram}
-          </p>
-          <p className="text-sm">
-            <span className="font-semibold">Categoria: </span>
-            {empresa.categoria?.tipo_servico || "—"}
-          </p>
+            <div className="p-4">
+              <p className="text-sm">
+                <span className="font-semibold">Localização: </span>
+                {empresa.localizacao}
+              </p>
+              <p className="text-sm">
+                <span className="font-semibold">Telefone: </span>
+                {empresa.telefone_contato}
+              </p>
+              <p className="text-sm break-words">
+                <span className="font-semibold">Instagram: </span>
+                {empresa.instagram}
+              </p>
+              <p className="text-sm">
+                <span className="font-semibold">Categoria: </span>
+                {empresa.categoria?.tipo_servico || "—"}
+              </p>
+            </div>
+          </div>
+
+          {showActions && ( // ← MOSTRA SOMENTE SE PERMITIDO
+            <div className="flex">
+              <Link
+                to={`/editarEmpresa/${empresa.id}`}
+                className="text-white bg-indigo-400 hover:bg-indigo-700 w-full flex items-center justify-center"
+              >
+                <button>Editar</button>
+              </Link>
+              <Link
+                to={`/deletarEmpresa/${empresa.id}`}
+                className="text-white bg-red-400 hover:bg-red-700 w-full flex items-center justify-center"
+              >
+                <button>Deletar</button>
+              </Link>
+            </div>
+          )}
         </div>
       </div>
-
-      {showActions && ( // ← MOSTRA SOMENTE SE PERMITIDO
-        <div className="flex">
-          <Link
-            to={`/editarEmpresa/${empresa.id}`}
-            className="text-white bg-indigo-400 hover:bg-indigo-700 w-full flex items-center justify-center"
-          >
-            <button>Editar</button>
-          </Link>
-          <Link
-            to={`/deletarEmpresa/${empresa.id}`}
-            className="text-white bg-red-400 hover:bg-red-700 w-full flex items-center justify-center"
-          >
-            <button>Deletar</button>
-          </Link>
-        </div>
-      )}
-    </div>
+    </section>
   );
 }
